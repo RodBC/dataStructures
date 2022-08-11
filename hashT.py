2da6264264459dbbab18e3afa5b9024c7c720211
RodBC <rodrigo.barros.castro@hotmail.com>
2022-08-11 15:15:57 -0300

    implementacao

--- hashT.py
+++ hashT.py
@@ -1,8 +1,8 @@
 class HashTable:
     def __init__(self, capacidade):
-        self.tabela = [None] * ((2 * capacidade) + 1)
+        self.tabela = [None] * capacidade
         
-    def hash_insert(T, chave) :
+    def hash_insert(self, T, chave) :
         i = 0
         j = chave % len(T)
 
@@ -17,23 +17,23 @@ class HashTable:
 
         return 'hashIsFull'
     
-    def hash_search(T, chave) :
+    def hash_search(self, T, chave) :
         i = 0
         j = chave % len(T)
-
         while i < len(T):
             if T[j] == None:
-                return None
-            if T[j] == chave:
+                print('NE')
+                return
+            elif T[j] == chave:
                 print(f'E: {j}') # found j
                 return
             else:
                 i += 1
                 j = (j + 1) % len(T)
 
-        print('NE')
 
-    def hash_capture(T, chave) :
+
+    def hash_capture(self, T, chave) :
         if T[chave]:
             print(f'A: {T[chave]}') # printa o valor no espaço de memoria dado
         else:
@@ -50,6 +50,7 @@ for x in range(n_commands):
   entrada = input().split()
   
   if entrada[0] == 'ADD':
+
     if myhash.hash_insert(myhash.tabela, int(entrada[1])) == 'hashIsFull':
         print('Toda memoria utilizada')
         break
